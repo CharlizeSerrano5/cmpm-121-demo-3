b7eb3ef61ea713b260cea929eaa629e5861379ff
Charlize <extrabutterychar@gmail.com>
2024-11-04 07:36:16 -0800

    L16 Dialog - Button implemented

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const button = document.createElement("button");
+button.innerHTML = "CLICK";
+
+button.addEventListener("click", ()=>{
+    alert("you clicked the button!");
+})
+
+app.append(button);
